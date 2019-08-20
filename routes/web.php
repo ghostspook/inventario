40608286938774b98a31fb70d2d9d6ab190c361c
Rafael Castillo <rafaelcastillo@outlook.com>
2019-08-20 16:00:53 -0500

    Adding Categories

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
 	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
-	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+    Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+
+    Route::get('categories', ['as' => 'categories.list', 'uses' => 'CategoryController@index']);
 });
 
