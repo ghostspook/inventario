85039c434222696c703fe16224c12ab5ba6e1ec0
Rafael Castillo <rafaelcastillo@outlook.com>
2019-08-20 17:37:59 -0500

    Adds products table

---
+++ database/migrations/2019_08_20_222528_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('sku')->unique();
+            $table->string('name');
+            $table->unsignedBigInteger('category_id');
+            $table->text('description');
+            $table->text('comments');
+            $table->decimal('stock', 10, 4);
+            $table->decimal('cost', 10, 4);
+            $table->decimal('price', 10, 4);
+            $table->string('rack');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
