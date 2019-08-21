ffb1b338535332b62f98673f4a6f2dcd16384064
ghostspook <pcastill@mac.com>
2019-08-20 23:57:13 -0500

    More DB seeding

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,7 +12,82 @@ class CategoriesTableSeeder extends Seeder
     public function run()
     {
         DB::table('categories')->insert([
+            'name' => 'ACCESORIOS CONECTORES PARA USB',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'ADAPTADORES USB',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AISLADOR DE RUIDO',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADOR DE FUENTE DE PODER',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADOR MARINOS PEQUEÑOS',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADORES 2 CANALES',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADORES 4 CANALES',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADORES 5 CANALES',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADORES MARINOS PEQUEÑOS',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
             'name' => 'AMPLIFICADORES MONOBLOCK',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADORES MONOBLOCK O CANAL 1',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'AMPLIFICADORES PRIME',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'APLICADORES',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'BAJOS',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'BAJOS AUTO SOUND',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'BAJOS MARINO',
             'created_at' => now(),
             'updated_at' => now()
         ]);
--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,12 +14,38 @@ class ProductsTableSeeder extends Seeder
         DB::table('products')->insert([
             'sku' => 'T2500-1BDCP',
             'name' => 'ROCKFORD 2500 RMS',
-            'category_id' => 1,
+            'category_id' => 10,
             'description' => '',
             'comments' => '',
             'stock' => 10,
             'cost' => 0,
             'price' => 1237.00,
+            'rack' => '',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('products')->insert([
+            'sku' => 'T1500-1BDCP',
+            'name' => 'ROCKFORD 1500 RMS',
+            'category_id' => 10,
+            'description' => '',
+            'comments' => '',
+            'stock' => 10,
+            'cost' => 0,
+            'price' => 621.00,
+            'rack' => '',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+        DB::table('products')->insert([
+            'sku' => 'T1000-1bdCP',
+            'name' => 'ROCKFORD 1000 RMS',
+            'category_id' => 10,
+            'description' => '',
+            'comments' => '',
+            'stock' => 10,
+            'cost' => 0,
+            'price' => 590.00,
             'rack' => '',
             'created_at' => now(),
             'updated_at' => now()
